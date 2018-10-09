fd801ba140ce128c666c83f70fe154894d713576
orinu <ori.nahum@gmail.com>
2018-10-09 14:50:48 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require(`express`);
+const hbs = require(`hbs`);
+const fs = require(`fs`);
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set(`view engine`, `hbs`)
+
+
+app.use((req,res, next) => {
+    const now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFileSync(`server.log`, log + `\n`);
+    next();
+});
+
+// app.use((rec, res, next) => {
+//     res.render(`maintenance.hbs`);
+// })
+
+app.use(express.static(__dirname +`/public`));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear())
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.get(`/`, (req, res) => {
+    res.render(`home.hbs`, {
+        pageTitle: `Home Page`,
+        textPage: `Welcome to my beoutiful website`
+    });
+});
+
+app.get(`/about`, (req, res) => {
+    res.render(`about.hbs`, {
+        pageTitle: `About Page`,
+    });
+});
+
+app.get(`/bad`, (req, res) => {
+    res.send({
+        errorMessage: `Balgan`
+    });
+});
+
+app.listen(2000, () => {
+    console.log(`Server is up`)
+});
